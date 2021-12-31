4d9dcb85b4fd15150329933daa5590fc9175d091
SomaSekimoto <0421.soma@gmail.com>
2021-12-31 18:46:12 +0900

    hello world vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    const message = "Hello World";
+    return {
+      message
+    };
+  }
+});
+</script>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/app', function () {
+    return view('app');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
